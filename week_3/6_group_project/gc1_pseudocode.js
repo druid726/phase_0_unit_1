6434e9a90c4b75c374391971ab53c8c5d5b35705
druid726 <pdoneill26@gmail.com>
2014-05-27 12:41:54 -0400

    Small edits

--- week_3/6_group_project/gc1_pseudocode.js
+++ week_3/6_group_project/gc1_pseudocode.js
@@ -2,12 +2,12 @@
 
 // Here is my part of the challenge: Translate tests into user stories
 
-1. I need a program that will give me the sum for a group of numbers. 
-2. I need a program that gives me the sum for all numbers in a line of odd length.
-3. I need a program that gives me the sum for all numbers in a line of even length.
-4. I need a program that gives me the average of a list of numbers.
-5. I need a program that gives me the average of an odd length list of numbers.
-6. I need a program that gives me the average of an even length list of numbers.
-7. I need a program that gives me the median value of a list of numbers.
-8. I need a program that gives me the median value of an odd length list of numbers.
-9. I need a program that gives me the median value of an even length list of numbers.
+1. I need a program that will give me the sum for a group of numbers that I can use anywhere else. 
+2. I need a program that gives me the sum for all numbers in a line of odd length that I can use anywhere else.
+3. I need a program that gives me the sum for all numbers in a line of even length that I can use anywhere else.
+4. I need a program that gives me the average of a list of numbers that I can use anywhere else.
+5. I need a program that gives me the average of an odd length list of numbers that I can use anywhere else.
+6. I need a program that gives me the average of an even length list of numbers that I can use anywhere else.
+7. I need a program that gives me the median value of a list of numbers that I can use anywhere else.
+8. I need a program that gives me the median value of an odd length list of numbers that I can use anywhere else.
+9. I need a program that gives me the median value of an even length list of numbers that I can use anywhere else.
