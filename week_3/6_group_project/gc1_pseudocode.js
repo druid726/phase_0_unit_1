7a3db40638e90b3768625f374715646534f3ae20
druid726 <pdoneill26@gmail.com>
2014-05-27 09:53:08 -0400

    Adding GC1 User Stories for Group Project

---
+++ week_3/6_group_project/gc1_pseudocode.js
@@ -0,0 +1,13 @@
+// My role in the group is: Person 1
+
+// Here is my part of the challenge: Translate tests into user stories
+
+1. I need a program that will give me the sum for a group of numbers. 
+2. I need a program that gives me the sum for all numbers in a line of odd length.
+3. I need a program that gives me the sum for all numbers in a line of even length.
+4. I need a program that gives me the average of a list of numbers.
+5. I need a program that gives me the average of an odd length list of numbers.
+6. I need a program that gives me the average of an even length list of numbers.
+7. I need a program that gives me the median value of a list of numbers.
+8. I need a program that gives me the median value of an odd length list of numbers.
+9. I need a program that gives me the median value of an even length list of numbers.
