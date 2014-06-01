d1eb36e033092cfa2feabd713e08fa50c736409a
druid726 <pdoneill26@gmail.com>
2014-06-01 04:17:39 -0400

    Solo Challenge Accepted and Reflected

--- week_3/5_design_cc_mission_solo_challenge/my_solution.js
+++ week_3/5_design_cc_mission_solo_challenge/my_solution.js
@@ -1,37 +1,52 @@
 // This is a solo challenge
 
 // Your mission description:
-// 
-// 
-// 
+// Break Captain Anya out of prison and kill Ogre
+// and escape prison
+// need Captain Anya and Tharin Objects
+// need move down, up, left and right methods and a break down door and kill Ogre methods
 
 // Pseudocode
-// 
-// 
-// 
-// 
-// 
+// break down the door to Captain Anya's prison cell
+// Tharin and Captain Anya walk down hall
+// Tharin kills Ogre
+// Captain Anya and Tharin escape the prison
 
 // Initial Code
+// var anya = {
+// 	name: "Captain Anya"
+// };
+var tharin = {
+	name: "Tharin"
+};
+function breakDownDoor() {};
+function killOgre() {};
+function moveRight() {};
+// function moveUp() {};
+// function moveDown() {};
+// function moveLeft() {};
 
 
-
-
-
+this.breakDownDoor();
+this.moveRight();
+this.moveRight();
+this.killOgre ();
+this.moveRight ();
 
 // Refactored Code
 
+//I think this code is fine.  I don't see where it can be Refactored.
 
 
 
 
 
 // Reflection
-// 
-// 
-// 
-// 
-// 
+// I'm not sure I did this correctly.  It seems too simplistic.  It was fun to do.
+// I love everything about the CodeCombat exercises - I'm a big kid at heart.  
+// Unfortunately I don't have the time to explore this further, that's one of
+// the most frustrating things about Phase 0 - there are so many cool links and 
+// websites to delve into but I don't have the time to explore them.
 // 
 // 
 // 
