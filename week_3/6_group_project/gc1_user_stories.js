91adb2263cf129dbf954472db73f1d62c3d9b93b
druid726 <pdoneill26@gmail.com>
2014-06-01 20:32:12 -0400

    My Reflection

--- week_3/6_group_project/gc1_user_stories.js
+++ week_3/6_group_project/gc1_user_stories.js
@@ -11,3 +11,11 @@
 7. I need a program that gives me the median value of a list of numbers that I can use anywhere else.
 8. I need a program that gives me the median value of an odd length list of numbers that I can use anywhere else.
 9. I need a program that gives me the median value of an even length list of numbers that I can use anywhere else.
+
+
+//My Reflection
+
+// I hated this exercise.  I felt left in the dark, there was no discussion -- it felt like we were playing telephone 
+// with the Mariana Trench.  Most of the tests failed with the exception of the function tests (#1,4,7).  I did not learn
+// anything new with regards to coding but I did get some experience with the JavaScript console in Chrome and Node in
+// the terminal.   
